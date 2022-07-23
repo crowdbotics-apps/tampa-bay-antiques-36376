0f3213882326f84a66ab44df2fcbc730828fb115
Crowdbotics <team@crowdbotics.com>
2022-07-23 19:45:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tampa_bay_antiques_36376",
+  url: "https://tampa_bay_antiques_36376.botics.co",
+  api: "https://tampa_bay_antiques_36376.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
